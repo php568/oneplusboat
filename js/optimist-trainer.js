7ea0d1ff3e5982b40f4b5e38ab1a6de3bb5b2cf2
php568 <tengfei.liu@seedeer.com>
2019-01-10 16:18:57 +0800

    修改长宽

--- js/optimist-trainer.js
+++ js/optimist-trainer.js
@@ -3,7 +3,8 @@
 
     layer.photos({
         photos: '#block-views-homepage-products-block',
-        anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+        anim: 5, //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+        closeBtn:1
     });
 
     //建造实例
--- js/parts.js
+++ js/parts.js
@@ -4,8 +4,6 @@
     layer.photos({
         photos: '#block-views-parts-products-block',
         anim: 5, //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
-        area: ['690px', '400px'],
-        shift: 1,
         closeBtn:1
     });
     $('.tb-megamenu ul.nav li.mega').mouseenter(function(){
