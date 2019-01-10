040e188dd95019d2e261c3e0ba44f931bea16a81
php568 <tengfei.liu@seedeer.com>
2019-01-10 16:12:54 +0800

    添加关闭按钮

--- js/parts.js
+++ js/parts.js
@@ -3,7 +3,10 @@
 
     layer.photos({
         photos: '#block-views-parts-products-block',
-        anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+        anim: 5, //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
+        area: ['500px', '300px'],
+        shift: 1,
+        closeBtn:1
     });
     $('.tb-megamenu ul.nav li.mega').mouseenter(function(){
         $(this).addClass('open');
