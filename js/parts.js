c2aedb0a8b0a42ec4278ff3e235b01ef25b451aa
php568 <tengfei.liu@seedeer.com>
2019-01-10 16:08:31 +0800

    添加英文id

--- js/parts.js
+++ js/parts.js
@@ -2,7 +2,7 @@
     var $ = layui.jquery, laytpl = layui.laytpl;
 
     layer.photos({
-        photos: '#block-views-parts-products-block', 
+        photos: '#block-views-parts-products-block',
         anim: 5 //0-6的选择，指定弹出图片动画类型，默认随机（请注意，3.0之前的版本用shift参数）
     });
     $('.tb-megamenu ul.nav li.mega').mouseenter(function(){
