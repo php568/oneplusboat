9783c921e9627fa1e81905aa2a8921ce58ede2d8
php568 <tengfei.liu@seedeer.com>
2019-01-24 14:23:12 +0800

    修改菜单动态显示

--- js/optimist-trainer.js
+++ js/optimist-trainer.js
@@ -25,10 +25,12 @@
     });
     $('.tb-megamenu-button').click(function() {
         if(parseInt($(this).parent().children('.nav-collapse').height())) {
-            $(this).parent().children('.nav-collapse').css({height: 0, overflow: 'hidden'});
+            // $(this).parent().children('.nav-collapse').css({height: 0, overflow: 'hidden'});
+            $('.tb-megamenu .nav-collapse').toggleClass('close');
         }
         else {
-            $(this).parent().children('.nav-collapse').css({height: 'auto', overflow: 'visible'});
+            // $(this).parent().children('.nav-collapse').css({height: 'auto', overflow: 'visible'});
+            $('.tb-megamenu .nav-collapse').toggleClass('open');
         }
     });
     $('.tb-megamenu .nav-collapse').toggleClass('open');
