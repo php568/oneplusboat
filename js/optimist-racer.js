d29375a256542381ff86b78f065db0f659a9e7f5
php568 <tengfei.liu@seedeer.com>
2019-01-24 14:39:18 +0800

    修改菜单动态显示

--- js/optimist-racer.js
+++ js/optimist-racer.js
@@ -24,16 +24,17 @@
         $(this).removeClass('open');
     });
     $('.tb-megamenu-button').click(function() {
-        if(parseInt($(this).parent().children('.nav-collapse').height())) {
-            // $(this).parent().children('.nav-collapse').css({height: 0, overflow: 'hidden'});
-            $(this).parent().children('.nav-collapse').removeClass('close');
-            $(this).parent().children('.nav-collapse').addClass('open');
-        }
-        else {
-            // $(this).parent().children('.nav-collapse').css({height: 'auto', overflow: 'visible'});
-            $(this).parent().children('.nav-collapse').removeClass('open');
-            $(this).parent().children('.nav-collapse').addClass('close');
-        }
+        // if(parseInt($(this).parent().children('.nav-collapse').height())) {
+        //     // $(this).parent().children('.nav-collapse').css({height: 0, overflow: 'hidden'});
+        //     $(this).parent().children('.nav-collapse').removeClass('close');
+        //     $(this).parent().children('.nav-collapse').addClass('open');
+        // }
+        // else {
+        //     // $(this).parent().children('.nav-collapse').css({height: 'auto', overflow: 'visible'});
+        //     $(this).parent().children('.nav-collapse').removeClass('open');
+        //     $(this).parent().children('.nav-collapse').addClass('close');
+        // }
+        $('.tb-megamenu .nav-collapse').toggleClass('open');
     });
     $('.tb-megamenu .nav-collapse').toggleClass('open');
 
